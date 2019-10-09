7179ca509d7c169519cd4046c9d6440de350238c
FKYnJYQ <loveress01@gmail.com>
2019-10-09 13:23:29 +0800

    fix(static): 修复`assets` -> `static`

--- clinic_django/settings.py
+++ clinic_django/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
