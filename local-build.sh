cccb81d18c29873aa1eab61d2e3dc37c53ce5c16
FKYnJYQ <loveress01@gmail.com>
2019-10-17 11:39:41 +0800

    fix: fix localbuild script

--- local-build.sh
+++ local-build.sh
@@ -1,4 +1,5 @@
 cd clinic_admin && yarn run build && cd ..
+cd clinic_docs && yarn docs:build && cd ..
 docker login docker.bitnp.net
-docker build -t "docker.bitnp.net/bitnp/clinic/clinic_django:local-build" .
-docker push docker.bitnp.net/bitnp/clinic/clinic_django:local-build
+docker build -t "docker.bitnp.net/bitnp/clinic/clinic_django:master" .
+docker push docker.bitnp.net/bitnp/clinic/clinic_django:master
