3057f621eee338dd5a4766fd178e3fc1bdd4b32a
FKYnJYQ <loveress01@gmail.com>
2019-10-17 19:39:31 +0800

    docs: add comment

--- clinic/authentication.py
+++ clinic/authentication.py
@@ -6,6 +6,9 @@ from rest_framework import exceptions
 
 class ApikeyAuthentication(authentication.BaseAuthentication):
     def authenticate(self, request):
+
+        # 这里可以增加识别 HOSTNAME 的操作 `request.get_host()`
+        # 不过目前似乎没有太大必要
         if not request.headers.get('X-API-KEY') == apikey:
             return None
         username = request.query_params.get('username')
