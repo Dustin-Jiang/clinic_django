78d2027800fffb0fccb6b9de6bdc5aea088ea8c0
FKYnJYQ <loveress01@gmail.com>
2019-10-19 14:18:23 +0800

    feat(crypto): 签名中加入时间戳
    没有对整体签名
    
    re #35.

--- clinic/utils.py
+++ clinic/utils.py
@@ -1,8 +1,10 @@
 from hashlib import md5
 from clinic_django.settings import apikey as server_side_apikey
+from datetime import datetime
 
 
 def verify_apikey(apikey: str, username: str) -> bool:
     _a = md5()
-    _a.update((server_side_apikey+username).encode())
+    _time = str(datetime.now().timestamp())[:9]
+    _a.update((server_side_apikey+username+_time).encode())
     return _a.hexdigest() == apikey
