d1942e37a3809ed5920cfed1c4236900ebfa05bf
everything411 <everything411@163.com>
2023-08-21 19:59:26 +0800

    migrate to github and dockerhub

--- local-build.sh
+++
@@ -1,5 +0,0 @@
-cd clinic_admin && yarn run build && cd ..
-cd clinic_docs && yarn docs:build && cd ..
-docker login docker.bitnp.net
-docker build -t "docker.bitnp.net/bitnp/clinic/clinic_django:master" .
-docker push docker.bitnp.net/bitnp/clinic/clinic_django:master
--- local-update-server.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -f docker-compose-deploy.yml pull web
-docker-compose -f docker-compose-deploy.yml stop web
-docker-compose -f docker-compose-deploy.yml rm -f web
-docker-compose -f docker-compose-deploy.yml up -d web
