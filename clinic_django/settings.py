0a5b42ec0eaceb55c2aeb28c6ece1f0fb4a837c8
everything411 <everything411@163.com>
2023-08-22 23:02:28 +0800

    set DEFAULT_AUTO_FIELD

--- clinic_django/settings.py
+++ clinic_django/settings.py
@@ -148,6 +148,10 @@ else:
     }
 
 
+# django 3.2 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
